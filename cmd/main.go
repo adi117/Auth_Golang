0e9f477aa15019edaf1b955a832d69e4df1d50b1
adi wicaksono <muhammadiwicaksono@gmail.com>
2025-08-05 19:22:57 +0700

    feat: add postgres db

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,16 @@
-package cmd
+package main
+
+import (
+	"auth/internal/config"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error to load .env file!")
+	}
+	config.InitDB()
+}
